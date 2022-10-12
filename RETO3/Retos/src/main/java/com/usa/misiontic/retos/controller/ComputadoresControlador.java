6c5778711e8a2e8c475f737b93021208d5ce669e
Monica Mendivelso <mmendivelso4@misena.edu.co>
2022-10-11 22:23:59 -0500

    seervicio

--- RETO3/Retos/src/main/java/com/usa/misiontic/retos/controller/ComputadoresControlador.java
+++ RETO3/Retos/src/main/java/com/usa/misiontic/retos/controller/ComputadoresControlador.java
@@ -1,2 +1,30 @@
-package com.usa.misiontic.retos.controller;public class ComputadoresControlador {
+package com.usa.misiontic.retos.controller;
+
+
+import com.usa.misiontic.retos.entities.Computadores;
+import com.usa.misiontic.retos.servicio.ComputadoresServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Computer")
+public class ComputadoresControlador {
+
+    @Autowired
+    private ComputadoresServicio ComputadoresServicio;
+
+    @GetMapping("/all")
+    public List<Computadores> getAll(){
+        return ComputadoresServicio.getAll();
+    }
+
+    @GetMapping("/save")
+    public Computadores save(@RequestBody Computadores p){
+        return ComputadoresServicio.save(p);
+    }
 }
--- RETO3/Retos/src/main/java/com/usa/misiontic/retos/servicio/ComputadoresServicio.java
+++ RETO3/Retos/src/main/java/com/usa/misiontic/retos/servicio/ComputadoresServicio.java
@@ -1,2 +1,78 @@
-package com.usa.misiontic.retos.servicio;public class ComputadoresServicio {
+package com.usa.misiontic.retos.servicio;
+
+import com.usa.misiontic.retos.entities.Computadores;
+import com.usa.misiontic.retos.repositorio.ComputadoresReposotorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ComputadoresServicio  {
+
+    @Autowired
+    private ComputadoresReposotorio ComputadoresRepositorio;
+
+    public List<Computadores> getAll(){
+        return ComputadoresRepositorio.getAll();
+    }
+
+    public Optional<Computadores> getComputadores(int id){
+        return ComputadoresRepositorio.getComputadores(id);
+    }
+
+    public Computadores save(Computadores p){
+        if(p.getId()==null){
+            return ComputadoresRepositorio.save(p);
+        }else{
+            Optional<Computadores> e = ComputadoresRepositorio.getComputadores(p.getId());
+            if (e.isPresent()){
+                return p;
+            }else{
+                return ComputadoresRepositorio.save(p);
+            }
+        }
+
+    }
+
+    public Computadores update(Computadores p){
+        if(p.getId()!=null){
+            Optional<Computadores> q = ComputadoresRepositorio.getComputadores(p.getId());
+            if(q.isPresent()){
+                if(p.getMarca()!=null){
+                    q.get().setMarca(p.getMarca());
+                }
+                if(p.getNombre()!=null){
+                    q.get().setNombre(p.getNombre());
+                }
+                if(p.getAno()!=null){
+                    q.get().setAno(p.getAno());
+                }
+                if(p.getDescripcion()!=null){
+                    q.get().setDescripcion(p.getDescripcion());
+                }
+                if(p.getCategoria()!=null){
+                    q.get().setCategoria(p.getCategoria());
+                }
+                ComputadoresRepositorio.save(q.get());
+                return q.get();
+            }else{
+                return p;
+            }
+        }else{
+            return p;
+        }
+    }
+
+    public boolean delete(int id){
+        boolean flag = false;
+        Optional<Computadores>p=ComputadoresRepositorio.getComputadores(id);
+        if(p.isPresent()){
+            ComputadoresRepositorio.delete(p.get());
+            flag =true;
+        }
+
+        return flag;
+    }
 }
